7a30c4a7215b4e24e82590140aaa992e153c570b
Victor <victorfalcon796@gmail.com>
2022-06-26 15:29:08 +0200

    Circles

--- RayTracer/src/Render.cpp
+++ RayTracer/src/Render.cpp
@@ -1,19 +1,9 @@
 #include "Render.h"
 
-void Renderer::Render()
-{
-	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++)
-	{
-		m_ImageData[i] = Walnut::Random::UInt();
-		m_ImageData[i] |= 0xff000000;
-	}
-
-	m_FinalImage->SetData(m_ImageData);
-}
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
-	if (!m_FinalImage)
+	if (m_FinalImage)
 	{
 		//No resize necesary
 		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
@@ -27,4 +17,46 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	}
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[width * height];
+}
+
+void Renderer::Render()
+{
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
+	{
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
+		{
+			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
+			coord = coord * 2.0f - 1.0f;
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+		}
+	}
+	m_FinalImage->SetData(m_ImageData);
+}
+
+uint32_t Renderer::PerPixel(glm::vec2 coord)
+{
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	float radius = 0.5f;
+	//rayDirection = glm::normalize(rayDirection);
+
+	//(bx^2 + by^2 + bz^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
+	// a = ray origin
+	// b = ray direction
+	// r = radius
+	// t = hit distance
+
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+
+	//Quadratic formula discriminant
+	//b^2 - 4ac
+	float discriminant = b * b - 4.0f * a * c;
+	
+	if (discriminant >= 0)
+		return 0xffff00ff;
+
+	return 0xff0000000;
 }
