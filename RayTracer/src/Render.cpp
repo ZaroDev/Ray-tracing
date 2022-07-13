efaefafc0f157e3ee770a8a2fff4225753a29812
Victor <victorfalcon796@gmail.com>
2022-07-13 11:47:50 +0200

    Aspect ratio

--- RayTracer/src/Render.cpp
+++ RayTracer/src/Render.cpp
@@ -21,7 +21,7 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 void Renderer::Render()
 {
-	float aspectRatio = m_FinalImage->GetWidth() / m_FinalImage->GetHeight();
+	float aspectRatio = (float)m_FinalImage->GetWidth() / (float)m_FinalImage->GetHeight();
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
